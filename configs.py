a22bd92b5a6a1045533ff51fcb8ab53d6a9603ee
Tuan Nguyen Minh <tuana9a@gmail.com>
2022-03-30 20:18:38 +0700

    update: add default config fallback for env

--- configs.py
+++ configs.py
@@ -3,6 +3,6 @@ import dotenv
 
 dotenv.load_dotenv(dotenv.find_dotenv())
 
-PORT = os.getenv("PORT")
-BIND = os.getenv("BIND")
-DEVICE = os.getenv("DEVICE")
+PORT = os.getenv("PORT") or 80
+BIND = os.getenv("BIND") or "0.0.0.0"
+DEVICE = os.getenv("DEVICE") or "cpu"
